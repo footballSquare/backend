be4dec9b9b5ee4e66fb7be0d04846c2eb7611ac8
Strangekim <114288288+Strangekim@users.noreply.github.com>
2025-03-05 20:26:45 +0900

    Master (#5)
    * 기본 셋팅 추가, match 서비스 몇가지
    
    * 매치, 팀 서비스 몇가지 구현
    
    * 매치 서비스들 작성 중

--- src/router/match/service.js
+++ src/router/match/service.js
@@ -2,18 +2,46 @@ const client = require("../../database/postgreSQL")
 const customError = require("../../util/customError")
 
 const {
+    getTeamMatchListSQL,
     getOpenMatchDataSQL,
     postOpenMatchSQL,
     postTeamMatchSQL,
-    deleteMatchSQL
+    putTeamMatchSQL,
+    closedMatchSQL,
+    deleteWaitlistSQL,
+    deleteMatchSQL,
+    getMatchDetailDataSQL,
+    getMatchParticipantListSQL,
+    checkMatchParticipationSQL,
+    postMatchParticipantSQL,
+    getMatchWaitListSQL,
+    deleteFromWaitListSQL,
+    insertIntoParticipantSQL,
+    postMatchWaitListSQL
 } = require("./sql")
 
-
-// 공개 매치 가져오기
-const getOpenMatchData = async (req,res,next) => {
+// 팀 매치 목록 보기
+const getTeamMatchList = async (req,res,next) => {
     const {page} = req.query
+    const {team_list_idx} = req.params
+
     try{
-        const result = await client.query(getOpenMatchDataSQL, [10, page])
+        const result = await client.query(getTeamMatchListSQL, [
+            team_list_idx,
+            page
+        ])
+        res.status(200).send({ match : result.rows })
+    } catch(e){
+        next(e)
+    }
+}
+
+// 공개 매치 목록 가져오기
+const getOpenMatchList = async (req,res,next) => {
+    const {page} = req.query
+    console.log(1)
+    try{
+        const result = await client.query(getOpenMatchDataSQL, [page])
         res.status(200).send({ match : result.rows })
     } catch(e){
         next(e)
@@ -28,7 +56,6 @@ const postOpenMatch = async (req,res,next) => {
         match_formation_idx,
         match_match_participation_type,
         match_type_idx,
-        match_match_attribute,
         match_match_start_time,
         match_match_duration
     } = req.body
@@ -39,6 +66,33 @@ const postOpenMatch = async (req,res,next) => {
             match_formation_idx,
             match_match_participation_type,
             match_type_idx,
+            match_match_start_time,
+            match_match_duration
+        ])
+        res.status(200).send({})
+    } catch(e){
+        next(e)
+    }
+}
+
+// 팀 매치 수정하기
+const putTeamMatch = async (req,res,next) => {
+    const {team_list_idx} = req.params
+    const {
+        player_list_idx,
+        match_match_idx,
+        match_match_participation_type,
+        match_match_attribute,
+        match_match_start_time,
+        match_match_duration
+    } = req.body
+
+    try{
+        await client.query(putTeamMatchSQL, [
+            match_match_idx,
+            team_list_idx,
+            player_list_idx,
+            match_match_participation_type,
             match_match_attribute,
             match_match_start_time,
             match_match_duration
@@ -49,6 +103,38 @@ const postOpenMatch = async (req,res,next) => {
     }
 }
 
+// 매치 마감하기
+const closedMatch = async (req,res,next) => {
+    const {match_match_idx} = req.params
+    const {
+        player_list_idx
+    } = req.body
+
+    try{
+        // 트랜잭션으로 매치를 먼저 마감, 대기자 목록 삭제
+        await client.query("BEGIN");
+        const closedMatchResult = await client.query(closedMatchSQL, [
+            match_match_idx,
+            player_list_idx
+        ])
+
+        if (closedMatchResult.rowCount === 0) {
+            throw customError(404, `해당 매치가(이) 존재하지 않습니다.`);
+        }
+
+        await client.query(deleteWaitlistSQL, [match_match_idx]);
+        await client.query("COMMIT");
+
+        res.status(200).send({})
+    } catch(e){
+        await client.query("ROLLBACK");
+        res.status(500).send({ 
+            message: "매치 마감 중 오류 발생", error: error.message 
+        });
+    }
+}
+
+// 팀 매치 만들기
 const postTeamMatch = async (req,res,next) => {
     const {team_list_idx} = req.params
     const {
@@ -90,8 +176,131 @@ const deleteMatch = async (req,res,next) => {
     }
 }
 
+// 매치 세부정보 가져오기
+const getMatchDetailData = async (req,res,next) => {
+    const {match_match_idx} = req.params
+
+    try{
+        const result = await client.query(getMatchDetailDataSQL, [match_match_idx])
+        res.status(200).send({ match : result.rows })
+    } catch(e){
+        next(e)
+    }
+}
+
+// 매치 참여자 목록 가져오기
+const getMatchParticipantList = async (req,res,next) => {
+    const {match_match_idx} = req.params
+
+    try{
+        const result = await client.query(getMatchParticipantListSQL, [match_match_idx])
+        res.status(200).send({ match_participant : result.rows })
+    } catch(e){
+        next(e)
+    }
+}
+
+// 매치 참여 대기자 목록 가져오기
+const getMatchWaitList = async (req,res,next) => {
+    const {match_match_idx} = req.params
+
+    try{
+        const result = await client.query(getMatchWaitListSQL, [match_match_idx])
+        const rows = result.rows;
+
+        const matchWaitlist = {};
+        rows.forEach(row => {
+            if (!matchWaitlist[row.match_position_idx]) {
+                matchWaitlist[row.match_position_idx] = [];
+            }
+            matchWaitlist[row.match_position_idx].push({
+                player_list_idx: row.player_list_idx,
+                player_list_nickname: row.player_list_nickname,
+                player_list_url: row.player_list_url
+            });
+        });
+
+        res.status(200).send({ match_waitlist : matchWaitlist })
+    } catch(e){
+        next(e)
+    }
+}
+
+// 매치 참여 승인하기
+const waitApproval = async (req,res,next) => {
+    const {match_match_idx} = req.params
+    const {
+        player_list_idx,
+        match_position_idx
+    } = req.query
+
+    try{
+        // 트랜잭션으로 매치를 먼저 마감, 대기자 목록 삭제
+        await client.query("BEGIN");
+
+        const deleteResult = await client.query(deleteFromWaitListSQL, [
+            match_match_idx,
+            player_list_idx,
+            match_position_idx
+        ])
+        if (deleteResult.rowCount === 0) {
+            throw customError(404, `해당 선수가(이) 존재하지 않습니다.`);
+        }
+
+        const insertResult = await client.query(insertIntoParticipantSQL, [
+            match_match_idx, 
+            player_list_idx, 
+            match_position_idx
+        ]);
+
+        if (insertResult.rowCount === 0) {
+            throw customError(404, `특정 데이터가(이) 존재하지 않습니다.`);
+        }
+
+        await client.query("COMMIT");
+
+        res.status(200).send({})
+    } catch(e){
+        await client.query("ROLLBACK");
+        res.status(500).send({ 
+            message: "매치 참여 승인 중 오류 발생", e : e.message 
+        });
+    }
+}
+
+// 공개 매치 참여하기
+const joinOpenMatch = async (req,res,next) => {
+    const {match_match_idx} = req.params
+    const {match_position_idx,player_list_idx} = req.body
+
+    try{
+        const result = await client.query(checkMatchParticipationSQL, [match_match_idx])
+        let sql
+
+        result.rows[0].match_match_participation_type == 0 ? sql = postMatchWaitListSQL : sql = postMatchParticipantSQL
+
+        await client.query(sql, [
+            match_match_idx,
+            player_list_idx,
+            match_position_idx
+        ])
+        res.status(200).send({})
+    } catch(e){
+        next(e)
+    }
+}
+
 module.exports = {
-    getOpenMatchData,
+    getTeamMatchList,
+    getOpenMatchList,
     postOpenMatch,
+    putTeamMatch,
+    closedMatch,
     postTeamMatch,
-    deleteMatch}
+    deleteMatch,
+    getMatchDetailData,
+    getMatchParticipantList,
+    getMatchWaitList,
+    waitApproval,
+    joinOpenMatch
+}
--- src/router/match/sql.js
+++ src/router/match/sql.js
@@ -1,6 +1,28 @@
-const getOpenMatchDataSQL =
+// 팀 매치 목록 보기
+const getTeamMatchListSQL = 
 `
 SELECT 
+    match.match.match_match_idx,
+    match.match.player_list_idx,
+    player.list.player_list_nickname,
+    player.list.player_list_profile_image,
+    match.match.match_match_participation_type,
+    match.match.match_type_idx,
+    match.match.match_match_attribute,
+    match.match.common_status_idx,
+    match.match.match_match_start_time,
+    match.match.match_match_duration
+FROM match.match
+JOIN player.list ON match.match.player_list_idx = player.list.player_list_idx
+WHERE match.match.team_list_idx = $1
+ORDER BY match.match.match_match_start_time DESC
+LIMIT 10 OFFSET $2;
+`
+
+// 공개 매치 목록 보기
+const getOpenMatchDataSQL =
+`
+SELECT
     m.match_match_idx,
     m.team_list_idx,
     t.team_list_name,
@@ -25,11 +47,12 @@ WHERE
 ORDER BY 
     m.match_match_start_time DESC
 LIMIT 
-    $1 
+    10
 OFFSET 
-    $2;
+    $1;
 `
 
+// 공방 매치 생성하기
 const postOpenMatchSQL =
 `
 INSERT INTO match.match (
@@ -41,9 +64,44 @@ INSERT INTO match.match (
     match_match_attribute,
     match_match_start_time,
     match_match_duration
-) VALUES ($1, NULL, $2, $3, $4, $5,  $6, $7);
+) VALUES ($1, NULL, $2, $3, $4, 0, $5, $6);
 `
 
+// 팀 매치 수정하기
+const putTeamMatchSQL = 
+`
+UPDATE match.match
+SET 
+    match_match_participation_type = $4,
+    match_match_attribute = $5,
+    match_match_start_time = $6,
+    match_match_duration = $7
+WHERE match_match_idx = $1
+AND team_list_idx = $2
+AND player_list_idx = $3;
+`
+
+// 매치 마감하기
+const closedMatchSQL = 
+`
+UPDATE 
+    match.match
+SET 
+    common_status_idx = 1
+WHERE 
+    match_match_idx = $1
+AND 
+    player_list_idx = $2;
+`
+
+// 매치 대기자 목록 삭제하기
+const deleteWaitlistSQL =
+`
+DELETE FROM match.waitlist
+WHERE match_match_idx = $1;
+`
+
+// 팀 매치 생성하기
 const postTeamMatchSQL = 
 `
 INSERT INTO match.match (
@@ -55,20 +113,172 @@ INSERT INTO match.match (
     match_match_attribute,
     match_match_start_time,
     match_match_duration
-) VALUES ($1, $2, $3, $4, $5,  $6, $7,$8);
+) VALUES ($1, $2, $3, $4, $5,  $6, $7, $8);
 `
 
+// 매치 삭제하기
 const deleteMatchSQL = 
 `
 DELETE FROM 
     match.match 
 WHERE match_match_idx = $1;
-
 `
 
+// 매치 세부 정보 가져오기
+const getMatchDetailDataSQL =
+`
+SELECT 
+    match.match.match_match_idx,
+    match.match.player_list_idx,
+    player.list.player_list_nickname,
+    player.list.player_list_profile_image,
+    match.match.match_formation_idx,
+    match.formation.match_position_idxs,
+    match.match.match_match_participation_type,
+    match.match.match_type_idx,
+    match.match.match_match_attribute,
+    match.match.common_status_idx,
+    match.match.match_match_start_time,
+    match.match.match_match_duration
+FROM match.match
+JOIN player.list ON match.match.player_list_idx = player.list.player_list_idx
+JOIN match.formation ON match.match.match_formation_idx = match.formation.match_formation_idx
+WHERE match.match.match_match_idx = $1;
+`
+
+// 매치 참여자 목록 가져오기
+const getMatchParticipantListSQL =
+`
+SELECT 
+    match.participant.player_list_idx,
+    player.list.player_list_nickname,
+    player.list.player_list_profile_image,
+    match.participant.match_position_idx
+FROM match.participant
+JOIN player.list ON match.participant.player_list_idx = player.list.player_list_idx
+WHERE match.participant.match_match_idx = $1
+ORDER BY match.participant.match_position_idx ASC;
+`
+
+// 매치의 참여 속성을 가져오는 SQL
+const checkMatchParticipationSQL =
+`
+SELECT 
+    match.match.match_match_participation_type
+FROM match.match
+WHERE match.match.match_match_idx = $1;
+`
+
+// 매치에 참여하는 SQL
+const postMatchParticipantSQL =
+`
+WITH player_data AS (
+    SELECT player_list_nickname 
+    FROM player.list 
+    WHERE player_list_idx = $2
+),
+match_time AS (
+    SELECT 
+        match.match.match_match_start_time, 
+        match.match.match_match_start_time + match.match.match_match_duration AS match_end_time
+    FROM match.match 
+    WHERE match.match.match_match_idx = $1
+)
+INSERT INTO match.participant (
+    match_match_idx,
+    player_list_idx,
+    match_position_idx,
+    player_list_nickname,
+    match_time_range
+) VALUES ($1, $2, $3, 
+    (SELECT player_list_nickname FROM player_data),
+    tstzrange(
+        (SELECT match_match_start_time FROM match_time), 
+        (SELECT match_end_time FROM match_time), 
+        '[)'
+    )
+);
+`
+
+// 매치 참여 대기자 목록 가져오기 
+const getMatchWaitListSQL =
+`
+SELECT 
+    match.waitlist.match_position_idx,
+    player.list.player_list_idx,
+    player.list.player_list_nickname,
+    player.list.player_list_profile_image AS player_list_url
+FROM match.waitlist
+JOIN player.list ON match.waitlist.player_list_idx = player.list.player_list_idx
+WHERE match.waitlist.match_match_idx = $1
+ORDER BY match.waitlist.match_position_idx, player.list.player_list_idx;
+`
+
+// 특정 선수 대기자 목록에서 삭제
+const deleteFromWaitListSQL =
+`
+DELETE FROM 
+    match.waitlist
+WHERE 
+    match_match_idx = $1
+AND 
+    player_list_idx = $2
+AND 
+    match_position_idx = $3
+RETURNING player_list_idx;
+`
+
+// 대기자 목록에서 참여자로 변환하는 SQL
+const insertIntoParticipantSQL = `
+INSERT INTO match.participant (
+    match_match_idx,
+    player_list_idx,
+    match_position_idx,
+    player_list_nickname,
+    match_time_range
+)
+SELECT 
+    $1, 
+    $2, 
+    $3, 
+    player.list.player_list_nickname,
+    tstzrange(
+        match.match.match_match_start_time, 
+        match.match.match_match_start_time + match.match.match_match_duration,
+        '[)'
+    )
+FROM player.list
+JOIN match.match ON match.match.match_match_idx = $1
+WHERE player.list.player_list_idx = $2
+RETURNING match_match_idx, player_list_idx, match_position_idx;
+`;
+
+// 매치에 참여 대기 등록하는 SQL
+const postMatchWaitListSQL =
+`
+INSERT INTO match.waitlist (
+    match_match_idx,
+    player_list_idx,
+    match_position_idx
+) VALUES ($1, $2, $3);
+`
+
+
 module.exports = {
+    getTeamMatchListSQL,
     getOpenMatchDataSQL,
     postOpenMatchSQL,
+    putTeamMatchSQL,
+    closedMatchSQL,
+    deleteWaitlistSQL,
     postTeamMatchSQL,
-    deleteMatchSQL
+    deleteMatchSQL,
+    getMatchDetailDataSQL,
+    getMatchParticipantListSQL,
+    checkMatchParticipationSQL,
+    postMatchParticipantSQL,
+    getMatchWaitListSQL,
+    deleteFromWaitListSQL,
+    insertIntoParticipantSQL,
+    postMatchWaitListSQL
 }
