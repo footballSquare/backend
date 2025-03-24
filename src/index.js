1a65c2052618516f46d798e6b43aa79218a038d3
Strangekim <roger345@naver.com>
2025-03-24 23:01:28 +0900

    cors IP 타겟

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-
-app.use(cors());
-
 require("dotenv").config();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",       // Vite 기본 포트
+      "http://localhost:3000",       // CRA, Next.js 등
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:3000",
+    ],
+    credentials: true,
+  })
+);
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
