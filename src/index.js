b271bd7d98cf5421889690a2ddcad6e0b1e48220
Strangekim <roger345@naver.com>
2025-04-06 00:41:16 +0900

    cors fix

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ const cors = require('cors');
 const app = express();
 require("dotenv").config();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",       // Vite 기본 포트
-      "http://localhost:3000",       // CRA, Next.js 등
-      "http://127.0.0.1:5173",
-      "http://127.0.0.1:3000",
-      "https://footballsquare.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "https://footballsquare.co.kr", 
+  credentials: true,
+}));
 
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
