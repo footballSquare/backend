dbe90e853783bcaccbab76d436602358c3943e21
GaeGeol Kim <98882970+Kimgaegeol@users.noreply.github.com>
2025-04-17 16:28:35 +0900

    Middlewareupdate (#38)
    * update API
    
    * middleware update
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-4-106.ap-northeast-2.compute.internal>
    Co-authored-by: Strangekim <114288288+Strangekim@users.noreply.github.com>

--- src/middleware/checkCondition.js
+++ src/middleware/checkCondition.js
@@ -143,6 +143,7 @@ const checkMatchEnded = () => {
 const checkMatchNotStarted = () => {
   return async (req, res, next) => {
     const { match_match_start_time } = req.matchInfo;
+    
     try {
       const matchStartTime = new Date(match_match_start_time);
       const now = new Date();
@@ -698,11 +699,9 @@ const checkIfChampionshipMatchOnly = () => {
   return (req, res, next) => {
     try {
       const { match_match_attribute } = req.matchInfo;
-
       if (match_match_attribute != MATCH_ATTRIBUTE.CHAMPIONSHIP) {
         throw customError(403, "이 기능은 대회 매치에서만 사용할 수 있습니다.");
       }
-
       next();
     } catch (e) {
       next(e);
