25aad25d4671c510706dd21f723346ce728bc820
Strangekim <roger345@naver.com>
2025-05-23 15:08:22 +0900

    팀명 정규표현식 변경

--- src/constant/regx.js
+++ src/constant/regx.js
@@ -4,7 +4,7 @@ const regMatchDuration = /^(30 minutes|1 hour|1 hour 30 minutes|2 hours)$/;
 const regMatchDatetime =
   /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]) ([01]\d|2[0-3]):(00|30):00$/;
 
-const regTeamName = /^[가-힣a-zA-Z]{1,20}$/; // 영어 한글만 가능, 최대 20글자
+const regTeamName = /^[가-힣a-zA-Z0-9\s()\[\]\-_]{1,20}$/; // 영어 한글만 가능, 최대 20글자
 
 const regTeamShortName = /^[A-Za-z]{3}$/; // 영문 3글자만 가능 (1글자, 2글자 불가)
 const regTeamAnnouncement = /^.{1,500}$/; // 최대 500글자까지 가능
