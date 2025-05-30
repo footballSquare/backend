57ce4d61df967211f47272dbb431741a7e7ffee1
Strangekim <114288288+Strangekim@users.noreply.github.com>
2025-05-30 19:10:12 +0900

    Yhk (#73)
    * 메시지 예외처리 추가
    
    * 팀 목록 랜덤으로 출력, 커뮤니티 게시글 작성 로직 API 추가
    
    * 종료된 매치 참여되지 않게 수정
    
    * 중복 가입 신청 예외처리 추가
    
    * account 수정
    
    * 공개매치 팀 매치 참여시 예외처리 추가
    
    * 게시글 작성 로직 수정
    
    * 오류 수정
    
    * 팀 매치 공개/비공개 매치로 변경
    
    * 게시글 생성 idx 반환
    
    * 댓글 idx 반환
    
    * 매치 제목 추가
    
    * 댓글 수정, 삭제 수정
    
    * 게시글, 댓글 시간 수정
    
    * 댓글,게시글 내용 정규표현식 수정
    
    * RT 발급 도메인 수정, 매치 해제하기 로직 수정
    
    * 계정 정규표현식 수정
    
    * 비밀번호 정규표현식 다시 수정
    
    * 내 정보 수정하기 미들웨어 추가
    
    * 팀 생성하기, 매치 대기자 수정
    
    * 팀명 정규표현식 변경
    
    * 팀 공지 줄바꿈 추가
    
    * 대회 매치 마감하기 수정
    
    * 팀 연혁 수정, 대회 매치 삭제 수정, 대회 매치 생성시 인덱스 반환
    
    * 팀장은 탈퇴가 불가능하도록 ㅅ ㅜ정
    
    * 팀 스탯 수정하기 키값 변경
    
    * 스탯 입력, 증빙자료 입력 로직 수정
    
    * 팀 채팅 socket 서버 구축 중

---
+++ chat/index.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const { createAdapter } = require('@socket.io/redis-adapter');
+const { createClient } = require('ioredis');
+const jwt = require('jsonwebtoken');
+
+const registerSocketEvents = require('./socket');
+const customError = require("./customError")
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    // origin: 'https://footballsquare.co.kr',
+    // credentials: true,
+    origin: '*', // 개발 중 전체 허용
+    credentials: false 
+  }
+});
+
+// Redis 클라이언트 2개 (Pub용 / Sub용)
+const pubClient = createClient({ 
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+});
+const subClient = pubClient.duplicate();
+
+// Redis 어댑터 설정
+io.adapter(createAdapter(pubClient, subClient));
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth?.token;
+
+  if (!token) return next(customError(401, '토큰 없음'));
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    socket.data.user = decoded;
+    next();
+  } catch (e) {
+    console.error("JWT verify 에러:", e.message);
+    return next(customError(403, '토큰 유효하지 않습니다'));
+  }
+});
+
+
+registerSocketEvents(io);
+
+const PORT = process.env.PORT
+
+httpServer.listen(PORT, () => {
+  console.log(`${PORT}번 포트에서 채팅 서버 실행 중`);
+});
